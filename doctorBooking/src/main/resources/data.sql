a1aab17b1b263530c415e56aa7992ddbcbb84992
AntonyAjay123 <64046806+AntonyAjay123@users.noreply.github.com>
2023-03-07 01:55:03 +0530

    Delete doctorBooking directory
    "deleted"

--- doctorBooking/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into doctor(doctor_name,experience,specialized_in) values(
-'ABC','7 years','heart');
-insert into doctor(doctor_name,experience,specialized_in) values(
-'XYZ','10 years','pediatrician');
-
